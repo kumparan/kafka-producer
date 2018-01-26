70462de3f008d2a091c7bea41fad64955b45c150
albert-widi <albert.widiatmoko@gmail.com>
2018-01-26 19:06:10 +0700

    Add kafka producer

--- kafka/kafka.go
+++ kafka/kafka.go
@@ -1,9 +1,20 @@
 package kafka
 
-func Init() {
+import (
+	"github.com/Shopify/sarama"
+)
 
+var kafkaProducer *sarama.SyncProducer
+
+type Message struct {
+	Topic   string `json:"topic"`
+	Content string `json:"content"`
 }
 
-func Publish() {
+func Init(producer *sarama.SyncProducer) {
+	kafkaProducer = producer
+}
 
+func Publish(msg Message) error {
+	return nil
 }
--- main.go
+++ main.go
@@ -1,16 +1,51 @@
 package main
 
 import (
+	"flag"
 	"os"
 	"os/signal"
+	"strings"
 	"syscall"
 	"time"
 
+	"github.com/Shopify/sarama"
+	"github.com/kumparan/go-lib/flags"
 	"github.com/kumparan/go-lib/log"
 	"github.com/kumparan/go-lib/webserver"
+	"github.com/kumparan/kafka-producer/kafka"
 )
 
+type serviceFlags struct {
+	logLevel string
+	brokers  string
+}
+
+func (sf *serviceFlags) Parse(fs *flag.FlagSet, args []string) error {
+	fs.StringVar(&sf.logLevel, "log_level", "", "logging level")
+	fs.StringVar(&sf.brokers, "brokers", "", "broker addresses")
+	return fs.Parse(args)
+}
+
 func main() {
+	sf := serviceFlags{}
+	flags.Parse(&sf)
+	log.SetLevelString(sf.logLevel)
+
+	brokers := strings.Split(sf.brokers, ",")
+	if len(brokers) == 0 {
+		log.Fatal("no brokers found")
+	}
+
+	producerConcifg := sarama.NewConfig()
+	producerConcifg.Producer.RequiredAcks = sarama.WaitForAll
+	producerConcifg.Producer.Retry.Max = 10
+	producerConcifg.Producer.Return.Successes = true
+	producer, err := sarama.NewSyncProducer(brokers, producerConcifg)
+	if err != nil {
+		log.Fatalf("failed to create producer: %s", err.Error())
+	}
+	kafka.Init(&producer)
+
 	server := webserver.New(webserver.Options{
 		Address: ":9000",
 		Timeout: time.Second * 2,
@@ -20,6 +55,7 @@ func main() {
 	go func() {
 		fatalChan <- server.Run()
 	}()
+	registerServiceEndpoints(server.Router())
 
 	term := make(chan os.Signal, 1)
 	signal.Notify(term, os.Interrupt, syscall.SIGTERM)
